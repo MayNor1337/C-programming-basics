1c0aa7ed4ce87f5bd36f9241235d8cda43eaa7af
MayNor <wen.net@yandex.ru>
2022-09-24 13:43:07 +0300

    Update lab2

--- lab2/main.c
+++ lab2/main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
+double z1(double a)
+{
+    return cos(a) + sin(a) + cos(3.0 * a) + sin(3.0 * a);
+}
+
+double z2(double a)
+{
+    return 2.0 * sqrt(2.0) * sin(M_PI  + 2.0 * a);
+}
+
 int main() {
-    float z1 = 0, z2 = 0;
-    float a;
+    double a;
+    scanf("%lf", &a);
 
-    scanf("%f", &a);
-
-    z1 = cos(a) + sin(a) + cos(3.0 * a) + sin(3.0 * a);
-    z2 = 2.0 * sqrt(2.0) * sin(M_PI  + 2.0 * a);
-
-    printf("%f\n", z1);
-    printf("%f", z2);
+    printf("%f\n", z1(a));
+    printf("%f", z2(a));
 
     return 0;
 }
